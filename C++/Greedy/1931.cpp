0010d05c6dff8fa7027d78be73f34a9bbe1be15d
haekang <ydjm1027@naver.com>
2021-01-26 14:49:18 +0900

    Create 1931.cpp

---
+++ C++/Greedy/1931.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+#define MAX 100001
+
+using namespace std;
+
+int n;
+int next_time = 0, ans = 0;
+vector<pair<int, int>> time;
+
+int main() {
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		int start, end;
+		scanf("%d %d", &start#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+
+#define MAX 100001
+
+using namespace std;
+
+int n;
+int next_time = 0, ans = 0;
+vector<pair<int, int>> time;
+
+int main() {
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		int start, end;
+		scanf("%d %d", &start, &end);
+		time.push_back(make_pair(end, start));
+	}
+
+	sort(time.begin(), time.end());
+
+	for (int j = 0; j < time.size(); j++) {
+		int meet_start = time[j].second;
+		int meet_end = time[j].first;
+		if (next_time <= meet_start) {
+			next_time = meet_end;
+			ans++;
+		}
+	}
+
+	printf("%d", ans);
+	
+	return 0;
+}, &end);
+		time.push_back(make_pair(end, start));
+	}
+
+	sort(time.begin(), time.end());
+
+	for (int j = 0; j < time.size(); j++) {
+		int meet_start = time[j].second;
+		int meet_end = time[j].first;
+		if (next_time <= meet_start) {
+			next_time = meet_end;
+			ans++;
+		}
+	}
+
+	printf("%d", ans);
+	
+	return 0;
+}
