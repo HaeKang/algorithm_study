b8e240c7d129df63de0076275e2bd7ae1b6074b0
haekang <ydjm1027@naver.com>
2021-01-26 23:13:39 +0900

    Create min_max.sql

---
+++ SQL/min_max.sql
@@ -0,0 +1,8 @@
+SELECT max(datetime) as 시간 from animal_ins;
+
+SELECT min(datetime) as 시간 from animal_ins;
+
+SELECT count(animal_id) as count from animal_ins;
+
+SELECT count(distinct name) as count from animal_ins;
+
