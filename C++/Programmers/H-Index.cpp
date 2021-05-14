d0b96099e3b7aa1293b8c1ae5a604cad9f42cdd3
haekang <ydjm1027@naver.com>
2021-05-14 16:52:56 +0900

    Create H-Index.cpp

---
+++ C++/Programmers/H-Index.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+    
+    if(citations[0] == 0){
+        return 0;
+    }
+    
+    for(int i=0;i <citations.size(); i++){
+        if(citations[i] <= i+1){
+            return citations[i];
+        }
+   }
+
+}
