30536655d96796f82bfd37b7e548ba6a58d672bc
haekang <ydjm1027@naver.com>
2021-07-06 16:29:32 +0900

    Update 5525.cpp

--- C++/5525.cpp
+++ C++/5525.cpp
@@ -34,3 +34,45 @@ int main() {
 	cout << ans << '\n';
 	return 0;
 }
+
+
+/*
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n, m, cnt, ans;
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(true);
+	cin.tie(nullptr);
+
+	cin >> n;	// OI의 개수
+	cin >> m;
+	cin >> s;
+
+
+	for (int i = 2; i <m; i++) {
+		if (s[i - 2] == 'O') {
+			continue;
+		}
+		
+
+		while (s[i - 1] == 'O' && s[i] == 'I') {
+			cnt++;
+			i += 2;
+			if (cnt == n) {
+				ans++;
+				cnt--;
+			}
+		}
+		
+		cnt = 0;
+
+	}
+
+	cout << ans << '\n';
+	return 0;
+}
+*/
