5613ee5f133381e4db997fd75fc0a18f61fc94bf
haekang <ydjm1027@naver.com>
2021-07-11 18:27:43 +0900

    Create isnull.sql

---
+++ SQL/isnull.sql
@@ -0,0 +1,7 @@
+SELECT animal_id from animal_ins where name is null;
+
+SELECT animal_id from animal_ins where name is not null;
+
+SELECT ANIMAL_TYPE, IFNULL(NAME,'No name')  AS 'NAME', SEX_UPON_INTAKE 
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
