d716dcde84206a339184caf061d6b507e06b6847
haekang <ydjm1027@naver.com>
2021-07-11 17:37:04 +0900

    Update group.sql

--- SQL/group.sql
+++ SQL/group.sql
@@ -2,5 +2,11 @@ SELECT animal_type, count(animal_type) as count from animal_ins where animal_typ
 
 SELECT name, count(name) as count from animal_ins group by name having count(name) >=2 order by name;
 
+select HOUR(datetime) as HOUR, count(animal_id) as count 
+from animal_outs 
+group by HOUR(datetime)
+having HOUR >= 9 and HOUR <=19
+order by HOUR;
+
 
 
