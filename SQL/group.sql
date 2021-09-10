3015f9715094dedb52b4d1aee71d9aeecb38a5da
haekang <ydjm1027@naver.com>
2021-09-10 22:54:59 +0900

    Update group.sql

--- SQL/group.sql
+++ SQL/group.sql
@@ -9,4 +9,8 @@ having HOUR >= 9 and HOUR <=19
 order by HOUR;
 
 
-
+set @hour := -1;
+select (@hour := @hour +1) as HOUR ,
+        (select count(*) from ANIMAL_OUTS where HOUR(DATETIME)=@hour) as COUNT
+from ANIMAL_OUTS
+where @hour < 23
