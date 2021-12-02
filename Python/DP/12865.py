a8e886f4bd97230b25431c83d61a5b241c13441c
haekang <ydjm1027@naver.com>
2021-12-02 21:23:48 +0900

    Create 12865.py

---
+++ Python/DP/12865.py
@@ -0,0 +1,22 @@
+import sys
+
+n, k = map(int, input().split())
+
+bags = [[0, 0]]
+dp = [[0] * (k+1) for _ in range(n+1)]
+
+for _ in range(n):
+    bags.append(list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, k+1):  # j = 가방무게
+        w = bags[i][0]
+        v = bags[i][1]
+
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-w] + v, dp[i-1][j])
+
+
+print(dp[n][k])
