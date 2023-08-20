2b0e01e3f585777516334813e22a2f192d283ee8
haekang <ydjm1027@naver.com>
2023-08-20 14:26:23 +0900

    Update 14940.py

--- Python/BFSDFS/14940.py
+++ Python/BFSDFS/14940.py
@@ -1,38 +1,38 @@
+'''
+https://www.acmicpc.net/problem/14940
+'''
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
-# row, col
-n, m = map(int, input().split())
+n, m = map(int, input().split())    # 행, 열
 
-# 0 : 갈수없음, 1 : 갈수있음, 2 : 목표지점
 arr = [list(map(int, input().split())) for _ in range(n)]
-check = [[-1] * (m) for _ in range(n)]  # -1 : 미방문
+checked = [[-1] * m for _ in range(n)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, 1, -1]
-
-# bfs
+dr = [0, 0, -1, 1]
+dc = [-1, 1, 0, 0]
 
 
-def bfs(start_x, start_y):
+def bfs(row, col):
     q = deque()
-
-    q.append([start_x, start_y])
-    check[start_x][start_y] = 0
+    q.append([row, col])
+    checked[row][col] = 0
 
     while q:
-        x, y = q.popleft()
+        now_r, now_c = q.popleft()
 
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            next_r = now_r + dr[i]
+            next_c = now_c + dc[i]
 
-            # 범위체크 & 방문체크
-            if (0 <= nx < n and 0 <= ny < m) and check[nx][ny] == -1 and arr[nx][ny] == 1:
-                check[nx][ny] = check[x][y] + 1
-                q.append([nx, ny])
+            if next_r < 0 or next_r >= n or next_c < 0 or next_c >= m:
+                continue
+
+            if checked[next_r][next_c] == -1 and arr[next_r][next_c] != 0:
+                q.append([next_r, next_c])
+                checked[next_r][next_c] = checked[now_r][now_c] + 1
 
 
 for i in range(n):
@@ -44,6 +44,8 @@ for i in range(n):
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 0:
-            check[i][j] = 0
-        print(check[i][j], end=" ")
+            print(0, end=" ")
+        else:
+            print(checked[i][j], end=" ")
+
     print()
