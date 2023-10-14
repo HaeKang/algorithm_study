644c433bc454b297098e514f94b5d3d55fa8b59d
haekang <ydjm1027@naver.com>
2023-10-14 14:54:14 +0900

    Update 6603.py

--- Python/조합순열/6603.py
+++ Python/조합순열/6603.py
@@ -1,44 +1,38 @@
 # https://www.acmicpc.net/problem/6603
-
 import sys
 from itertools import combinations
 input = sys.stdin.readline
 
-# 구현
-def make_combi(result):
-    if len(result) == 6:
-        combi_arr.append(result)
-        return
-    
-    start = 0
-    if len(result) != 0:
-        start = arr.index(result[-1]) + 1
-    
-    for i in range(start, len(arr)):
-        make_combi(result + [arr[i]])
+total_result = []
 
-arr = []
-combi_arr = []
+
+# def make_combi(result, arr):
+#     if len(result) == 6:
+#         total_result.append(result)
+#         return
+
+#     start = 0
+#     if len(result) != 0:
+#         start = arr.index(result[-1]) + 1
+
+#     for i in range(start, len(arr)):
+#         make_combi(result + [arr[i]], arr)
+
 
 while True:
     arr = list(map(int, input().split()))
-
-    k = int(arr[0])
-
-    if k == 0:
+    if arr[0] == 0:
         break
 
-    arr = arr[1:]
+    total_result.clear()
+    total_result = list(combinations(arr[1:], 6))
 
-    # 구현
-    combi_arr.clear()
-    make_combi([])
+    # make_combi([], arr[1:])
 
-    # 라이브러리
-    combi_arr = list(combinations(arr, 6))
-    
-    for combi in combi_arr:
-        for c in combi:
-            print(c, end = " ")
+    total_result.sort()
+
+    for total_r in total_result:
+        for r in total_r:
+            print(r, end=" ")
         print("")
     print("")
